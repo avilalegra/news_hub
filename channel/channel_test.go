76319a45a85e964d5253a5f301bb4c61e79236d2
Ricardo Avila <avilalegra@gmail.com>
2021-10-06 21:01:08 +0200

    test refactor
    segregates common samples

--- channel/channel_test.go
+++ channel/channel_test.go
@@ -108,24 +108,24 @@ var fetchChannelTests = []struct {
 }{
 
 	{
-		newMockedSource(parseChannelTests[0].channel.Link, parseChannelTests[0].xml),
-		parseChannelTests[0],
+		newMockedSource(chanSamples[0].channel.Link, chanSamples[0].xml),
+		chanSamples[0],
 	},
 	{
-		newMockedSource(parseChannelTests[1].channel.Link, parseChannelTests[1].xml),
-		parseChannelTests[1],
+		newMockedSource(chanSamples[1].channel.Link, chanSamples[1].xml),
+		chanSamples[1],
 	},
 	{
-		newMockedSource(parseChannelTests[2].channel.Link, parseChannelTests[2].xml),
-		parseChannelTests[2],
+		newMockedSource(chanSamples[2].channel.Link, chanSamples[2].xml),
+		chanSamples[2],
 	},
 	{
-		newMockedSource(parseChannelTests[3].channel.Link, parseChannelTests[3].xml),
-		parseChannelTests[3],
+		newMockedSource(chanSamples[3].channel.Link, chanSamples[3].xml),
+		chanSamples[3],
 	},
 	{
-		newMockedSource(parseChannelTests[4].channel.Link, parseChannelTests[4].xml),
-		parseChannelTests[4],
+		newMockedSource(chanSamples[4].channel.Link, chanSamples[4].xml),
+		chanSamples[4],
 	},
 	{
 		newMockedSource("some link", "<xml> invalid rss <xml>"),
@@ -137,7 +137,14 @@ var fetchChannelTests = []struct {
 	},
 }
 
-var parseChannelTests = []struct {
+var parseChannelTests = chanSamples
+
+var channelNewsTests = []Channel{
+	chanSamples[3].channel,
+	chanSamples[4].channel,
+}
+
+var chanSamples = []struct {
 	xml     string
 	channel Channel
 }{
@@ -207,44 +214,6 @@ var parseChannelTests = []struct {
 					Link:        `http://www.rtve.es/noticias/20210928/guia-restricciones-covid-nuevas-medidas-ocio-nocturno-hosteleria-aforos/2041269.shtml`,
 					Description: `<ul> <li>Repasa las principales medidas y restricciones frente a la COVID-19, comunidad a comunidad</li> <li><a href="https://www.rtve.es/noticias/20210928/coronavirus-covid-directo-espana-mundo-ultima-hora/2175601.shtml" target="_blank">Coronavirus: &uacute;ltima hora</a>&nbsp;|&nbsp;<a href="https://www.rtve.es/noticias/20210924/mapa-del-coronavirus-espana/2004681.shtml" target="_blank">Mapa de Espa&ntilde;a</a>&nbsp;|&nbsp;<a href="https://www.rtve.es/noticias/20210924/ocupacion-camas-covid-19-hospitales-espanoles/2042349.shtml" target="_blank">Hospitales y UCI</a></li> <li><a href="https://www.rtve.es/noticias/20210924/campana-vacunacion-espana/2062499.shtml" target="_blank">Vacunas en Espa&ntilde;a</a>&nbsp;|&nbsp;<a href="https://www.rtve.es/noticias/20210924/mapa-mundial-del-coronavirus/1998143.shtml" target="_blank">Mapa mundial&#8203;</a>&nbsp;|&nbsp;<a href="https://www.rtve.es/lab/vacunacion-espana-coronavirus/">Especial: La gran vacunaci&oacute;n</a></li> </ul><br/><a href="http://www.rtve.es/noticias/20210928/guia-restricciones-covid-nuevas-medidas-ocio-nocturno-hosteleria-aforos/2041269.shtml">Leer la noticia completa</a><img src="http://secure-uk.imrworldwide.com/cgi-bin/m?ci=es-rssrtve&cg=F-N-B-TENOTICI-TESESPE01-TELCO20VX&si=http://www.rtve.es/noticias/20210928/guia-restricciones-covid-nuevas-medidas-ocio-nocturno-hosteleria-aforos/2041269.shtml" alt=""/>`,
 				},
-			},
-		},
-	},
-}
-
-var channelNewsTests = []Channel{
-	{
-		Title:       `Phoronix`,
-		Link:        `https://www.phoronix.com/`,
-		Language:    `en-US`,
-		Description: `Linux Hardware Reviews & News`,
-		Items: []Item{
-			{
-				Title:       `AMD Posts Code Enabling "Cyan Skillfish" Display Support Due To Different DCN2 Variant`,
-				Link:        `https://www.phoronix.com/scan.php?page=news_item&px=AMD-Cyan-Skillfish-DCN-2.01`,
-				Description: `Since July we've seen AMD open-source driver engineers posting code for "Cyan Skillfish" as an APU with Navi 1x graphics. While initial support for Cyan Skillfish was merged for Linux 5.15, it turns out the display code isn't yet wired up due to being a different DCN2 variant for its display block...`,
-			},
-			{
-				Title:       `Linux 5.16 To Bring Initial DisplayPort 2.0 Support For AMD Radeon Driver (AMDGPU)`,
-				Link:        `https://www.phoronix.com/scan.php?page=news_item&px=AMDGPU-DP-2.0-Linux-5.16`,
-				Description: `A batch of feature updates was submitted today for DRM-Next of early feature work slated to come to the next version of the Linux kernel...`,
-			},
-		},
-	},
-	{
-		Title:       `Noticias en rtve.es`,
-		Link:        `http://www.rtve.es`,
-		Description: `RSS Tags`,
-		Items: []Item{
-			{
-				Title:       `Erupción en La Palma, en directo | La lava llega a 800 metros del mar y cambia de dirección al norte`,
-				Link:        `http://www.rtve.es/noticias/20210928/erupcion-palma-directo-lava-llega-800-metros-del-mar-cambia-direccion-norte/2175602.shtml`,
-				Description: `<ul> <li>Varios n&uacute;cleos poblacionales del municipio de Tazacorte han sido confinados</li> <li>La colada de lava podr&iacute;a llegar a la costa en las pr&oacute;ximas horas</li> </ul><br/><a href="http://www.rtve.es/noticias/20210928/erupcion-palma-directo-lava-llega-800-metros-del-mar-cambia-direccion-norte/2175602.shtml">Leer la noticia completa</a><img src="http://secure-uk.imrworldwide.com/cgi-bin/m?ci=es-rssrtve&cg=F-N-B-TENOTICI-TESESPE01-TES800089&si=http://www.rtve.es/noticias/20210928/erupcion-palma-directo-lava-llega-800-metros-del-mar-cambia-direccion-norte/2175602.shtml" alt=""/>`,
-			},
-			{
-				Title:       `Guía de restricciones COVID: nuevas medidas en ocio nocturno, hostelería y aforos, directo`,
-				Link:        `http://www.rtve.es/noticias/20210928/guia-restricciones-covid-nuevas-medidas-ocio-nocturno-hosteleria-aforos/2041269.shtml`,
-				Description: `<ul> <li>Repasa las principales medidas y restricciones frente a la COVID-19, comunidad a comunidad del municipio</li> <li><a href="https://www.rtve.es/noticias/20210928/coronavirus-covid-directo-espana-mundo-ultima-hora/2175601.shtml" target="_blank">Coronavirus: &uacute;ltima hora</a>&nbsp;|&nbsp;<a href="https://www.rtve.es/noticias/20210924/mapa-del-coronavirus-espana/2004681.shtml" target="_blank">Mapa de Espa&ntilde;a</a>&nbsp;|&nbsp;<a href="https://www.rtve.es/noticias/20210924/ocupacion-camas-covid-19-hospitales-espanoles/2042349.shtml" target="_blank">Hospitales y UCI</a></li> <li><a href="https://www.rtve.es/noticias/20210924/campana-vacunacion-espana/2062499.shtml" target="_blank">Vacunas en Espa&ntilde;a</a>&nbsp;|&nbsp;<a href="https://www.rtve.es/noticias/20210924/mapa-mundial-del-coronavirus/1998143.shtml" target="_blank">Mapa mundial&#8203;</a>&nbsp;|&nbsp;<a href="https://www.rtve.es/lab/vacunacion-espana-coronavirus/">Especial: La gran vacunaci&oacute;n</a></li> </ul><br/><a href="http://www.rtve.es/noticias/20210928/guia-restricciones-covid-nuevas-medidas-ocio-nocturno-hosteleria-aforos/2041269.shtml">Leer la noticia completa</a><img src="http://secure-uk.imrworldwide.com/cgi-bin/m?ci=es-rssrtve&cg=F-N-B-TENOTICI-TESESPE01-TELCO20VX&si=http://www.rtve.es/noticias/20210928/guia-restricciones-covid-nuevas-medidas-ocio-nocturno-hosteleria-aforos/2041269.shtml" alt=""/>`,
 			},
 		},
 	},
