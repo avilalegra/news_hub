84a2fc717cf0f1b9d26272e3bf61b480eb9dd226
Ricardo Avila <avilalegra@gmail.com>
2021-10-06 16:54:49 +0200

    news add

--- news/news.go
+++ news/news.go
@@ -15,5 +15,46 @@ type Extract struct {
 }
 
 type NewsProvider interface {
-	FetchNews() ([]*Extract, error)
+	FetchNews() ([]Extract, error)
+}
+
+var Register []Extract
+
+func Update(providers ...NewsProvider) (int, []error) {
+	recentNews := make([]Extract, 0, len(providers))
+	errors := make([]error, 0)
+	reschan := make(chan []Extract, 1)
+	errchan := make(chan []error, 1)
+
+	for _, p := range providers {
+		go func(prv NewsProvider, reschan chan<- []Extract, errchan chan<- []error) {
+			news, err := prv.FetchNews()
+			if err != nil {
+				errchan <- errors
+			} else {
+				reschan <- news
+			}
+		}(p, reschan, errchan)
+	}
+
+	for i := 0; i < len(providers); i++ {
+		select {
+		case news := <-reschan:
+			recentNews = append(recentNews, news...)
+		case errs := <-errchan:
+			errors = append(errors, errs...)
+		}
+	}
+
+	Register = recentNews
+
+	return len(recentNews), errors
+}
+
+func Add(extract Extract) {
+	Register = append(Register, extract)
+}
+
+func All() []Extract {
+	return Register
 }
