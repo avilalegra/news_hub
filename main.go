517b03d40ed82e4bb2e7f03a28a11eacd63d7526
Ricardo Avila <avilalegra@gmail.com>
2021-10-18 17:58:37 +0200

    demo purposes

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"avilego.me/recent_news/container"
 	"avilego.me/recent_news/persistence"
 	"context"
+	"time"
 )
 
 func main() {
@@ -14,4 +15,15 @@ func main() {
 	}()
 
 	container.GetCollector().Run()
+
+	// This is just for demo purposes
+	// Be sure to start containers: docker-compose up
+	// Run the project go run main.go
+	// Go to localhost:8081 and look for bd recent_news_test
+	// it would be populated every one minute with news
+	// from rss sources (see container.go).
+	// Keep in mind that you have to wait at least one minute when you run it
+	// and that if you run the tests the test db is overwritten
+	// Another type of news providers wil be included in the future
+	<-time.After(time.Minute * 10)
 }
