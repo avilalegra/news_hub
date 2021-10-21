c860494b0d4b1ef03d2b5687dac420c7839ca359
Ricardo Avila <avilalegra@gmail.com>
2021-10-21 18:13:50 +0200

    run server at application start

--- main.go
+++ main.go
@@ -3,27 +3,23 @@ package main
 import (
 	"avilego.me/recent_news/factory"
 	"avilego.me/recent_news/persistence"
+	"avilego.me/recent_news/server"
 	"context"
-	"time"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	defer func() {
 		if err := persistence.Client.Disconnect(context.TODO()); err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 	}()
 
 	factory.Collector().Run()
 
-	// This is just for demo purposes
-	// Be sure to start containers: docker-compose up
-	// Run the project go run main.go
-	// Go to localhost:8081 and look for bd recent_news_test
-	// it would be populated every one minute with news
-	// from rss sources (see container.go).
-	// Keep in mind that you have to wait at least one minute when you run it
-	// and that if you run the tests the test db is overwritten
-	// Another type of news providers wil be included in the future
-	<-time.After(time.Minute * 10)
+	log.Fatal(
+		http.ListenAndServe(os.Getenv("ServerAddr"), server.NewServerHttpHandler()),
+	)
 }
