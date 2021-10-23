546899c74d2d4ce4a3526bd474586fbe0f6149bb
Ricardo Avila <avilalegra@gmail.com>
2021-10-23 21:29:21 +0200

    more generic asserts in server handler search test

--- handler/server_handler_test.go
+++ handler/server_handler_test.go
@@ -5,7 +5,6 @@ import (
 	"avilego.me/recent_news/news"
 	"avilego.me/recent_news/persistence"
 	"github.com/stretchr/testify/assert"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -26,11 +25,8 @@ func TestApiSearchIntegration(t *testing.T) {
 	}
 
 	assert.Equal(t, 200, resp.StatusCode)
-	content, _ := io.ReadAll(resp.Body)
-	assert.Equal(t,
-		`{"Count":2,"Data":{"Sources":[{"Title":"Phoronix","Link":"https://www.phoronix.com/","Description":"Linux Hardware Reviews \u0026 News","Language":"en-US"}],"Previews":[{"Title":"AMD Posts Code Enabling \"Cyan Skillfish\" Display Support Due To Different DCN2 Variant","Link":"https://www.phoronix.com/scan.php?page=news_item\u0026px=AMD-Cyan-Skillfish-DCN-2.01","Description":"Since July we've seen AMD open-source driver engineers posting code for \"Cyan Skillfish\" as an APU with Navi 1x graphics. While initial support for Cyan Skillfish was merged for Linux 5.15, it turns out the display code isn't yet wired up due to being a different DCN2 variant for its display block...","SourceLink":"https://www.phoronix.com/"},{"Title":"Linux 5.16 To Bring Initial DisplayPort 2.0 Support For AMD Radeon Driver (AMDGPU)","Link":"https://www.phoronix.com/scan.php?page=news_item\u0026px=AMDGPU-DP-2.0-Linux-5.16","Description":"A batch of feature updates was submitted today for DRM-Next of early feature work slated to come to the next version of the Linux kernel...","SourceLink":"https://www.phoronix.com/"}]}}`,
-		string(content),
-	)
+	assert.NotEqual(t, 0, resp.ContentLength)
+	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
 }
 
 func loadDbFixtures() {
