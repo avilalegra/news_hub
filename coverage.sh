5834f1aa5b4be5d34fc6e4cfda34ac6ca5506f93
Ricardo Avila <avilalegra@gmail.com>
2021-10-23 21:21:42 +0200

    fix coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PKG_LIST=$(go list ./... | grep -v /vendor/ | tr '\n' ' ')
-go test -covermode=count -coverprofile coverage $PKG_LIST 
+GOFLAGS="-count=1" go test -covermode=count -coverprofile coverage $PKG_LIST  -p 1 ./...
 
 if [[ "$1" == "--html" ]]; then
   go tool cover -html=coverage
