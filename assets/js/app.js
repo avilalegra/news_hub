832778b82217c8943413349b10efadbab616438d
Ricardo Avila <avilalegra@gmail.com>
2021-11-11 19:07:18 +0100

    fixed header on scroll

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+$(function () {
+    appHeader().stickyOnScroll()
+})
+
+
+const appHeader = function () {
+    const header = {
+        fixed: false,
+        scrollTopLimit: 160,
+        $element: $("#header"),
+    }
+
+    header.stickyOnScroll = function () {
+        let self = this
+        $(window).scroll(function () {
+            let scroll = $(window).scrollTop();
+            if (scroll > self.scrollTopLimit && !self.fixed) {
+                self.$element.addClass("js-header-fixed")
+                self.fixed = true
+            } else if (scroll < self.scrollTopLimit && self.fixed) {
+                self.$element.removeClass("js-header-fixed")
+                self.fixed = false
+            }
+        })
+    }
+
+    return header
+}
